971030978ed8bee96357bb1488a8acb8e1c1e312
Sam <super8989@gmail.com>
2019-10-27 14:06:56 -0400

    using bind on Die component for onClick instead of fat arrow this.props.handleClick

--- src/Die.js
+++ src/Die.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		this.props.handleClick(this.props.idx);
+	}
+
 	render() {
 		return (
 			<button
 				className={"Die"}
 				style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-				onClick={() => this.props.handleClick(this.props.idx)}
+				onClick={this.handleClick}
+				//onClick={() => this.props.handleClick(this.props.idx)}
 			>
 				{this.props.val}
 			</button>
