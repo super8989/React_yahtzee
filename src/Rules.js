ca05a248d313e2c03f34edf4b366ddcf2db34f59
Sam <super8989@gmail.com>
2019-10-27 23:25:02 -0400

    added in small straight scoring in Rules.js

--- src/Rules.js
+++ src/Rules.js
@@ -78,9 +78,21 @@ class FullHouse extends Rule {
 }
 
 /** Check for small straights. */
+// 1234, 2345, 3456
+class SmallStraight extends Rule {
+	evalRoll = dice => {
+		const d = new Set(dice);
 
-class SmallStraight {
-	// TODO
+		// 234 + either 1 or 5
+		if (d.has(2) && d.has(3) && d.has(4) && (d.has(1) || d.has(5)))
+			return this.score;
+
+		// 345 + either 2 or 6
+		if (d.has(3) && d.has(4) && d.has(5) && (d.has(2) || d.has(6)))
+			return this.score;
+
+		return 0;
+	};
 }
 
 /** Check for large straights. */
@@ -120,7 +132,7 @@ const fourOfKind = new SumDistro({ count: 4 });
 const fullHouse = new FullHouse({ score: 25 });
 
 // small/large straights score as 30/40
-const smallStraight = "TODO";
+const smallStraight = new SmallStraight({ score: 30 });
 const largeStraight = new LargeStraight({ score: 40 });
 
 // yahtzee scores as 50
